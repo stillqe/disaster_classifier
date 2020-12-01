66231085bfbc29a7abe61e54ea6b05fdc86f2b61
Kyou Lee <stillqe@gmail.com>
2020-11-30 17:08:28 -0800

    refactoring

--- data/process_data.py
+++ data/process_data.py
@@ -51,31 +51,31 @@ def save_data(df, database_filename):
     df.to_sql('Disaster', engine, index=False)  
 
 
-def main():
-    if len(sys.argv) == 4:
+def run_etl_pipeline(messages_filepath, categories_filepath, database_filepath):
 
-        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+    print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+          .format(messages_filepath, categories_filepath))
+    df = load_data(messages_filepath, categories_filepath)
 
-        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-              .format(messages_filepath, categories_filepath))
-        df = load_data(messages_filepath, categories_filepath)
+    print('Cleaning data...')
+    df = clean_data(df)
 
-        print('Cleaning data...')
-        df = clean_data(df)
-        
-        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-        save_data(df, database_filepath)
-        
-        print('Cleaned data saved to database!')
-    
-    else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
-              'DisasterResponse.db')
+    print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+    save_data(df, database_filepath)
+
+    print('Cleaned data saved to database!')
+
+
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 4:
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+        run_etl_pipeline(messages_filepath, categories_filepath, database_filepath)
+    else:
+        print('Please provide the filepaths of the messages and categories ' \
+              'datasets as the first and second argument respectively, as ' \
+              'well as the filepath of the database to save the cleaned data ' \
+              'to as the third argument. \n\nExample: python process_data.py ' \
+              'disaster_messages.csv disaster_categories.csv ' \
+              'DisasterResponse.db')
